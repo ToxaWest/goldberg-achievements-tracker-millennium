c6b6bf96b9e9aef31a8943e507925808cc19c6b4
west <toxawest@gmail.com>
2026-03-05 23:50:28 +0200

    fix v10

---
+++ backend/main.lua
@@ -0,0 +1,8 @@
+-- GSE Achievements: Minimal Lua Backend
+function _load()
+    print("GSE Achievements: Backend successfully loaded.")
+end
+
+function _unload()
+    print("GSE Achievements: Backend unloading.")
+end
